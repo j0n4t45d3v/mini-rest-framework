785426576a0cd870b439c7a4309a7d79cf04a840
Jonatas <jonatas.melo@3lminformatica.com.br>
2025-12-22 13:33:16 -0300

    refactor Kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,7 +2,10 @@
 
 namespace Mini\Framework;
 
+use Mini\Framework\Core\_Internal\ControllerDiscovery;
 use Mini\Framework\Core\_Internal\Handlers\DefaultExceptionHandler;
+use Mini\Framework\Core\_Internal\RouterManager;
+use Mini\Framework\Core\_Internal\Utils\NormalizeUtils;
 use Mini\Framework\Core\Attributes\Body;
 use Mini\Framework\Core\Attributes\Controller;
 use Mini\Framework\Core\Attributes\Middleware;
@@ -13,9 +16,6 @@ use Mini\Framework\Core\Models\Request;
 use Mini\Framework\Core\Models\Response;
 use Mini\Framework\Http\MiddlewareHandlerAfter;
 use Mini\Framework\Http\MiddlewareHandlerBefore;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use ReflectionClass;
 use ReflectionMethod;
 use ReflectionParameter;
 use Throwable;
@@ -23,113 +23,79 @@ use Throwable;
 final class Kernel
 {
 
+  use NormalizeUtils;
+
   private const CACHE_DIR = "cache/";
   private const CACHE_ROUTE = self::CACHE_DIR . "routes.php";
+  private readonly ControllerDiscovery $controllerDiscovery;
+  private array $cache;
+  private bool $enableCache;
 
   public function __construct(
+      string $root = __DIR__,
       private readonly ExceptionHandler $exceptionHandler = new DefaultExceptionHandler()
-  ){}
-
-  public function registerControllers(string $directory = "src/Controller"): void
-  {
-    $controllerFiles = new RecursiveIteratorIterator(
-      new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS)
-    );
-
-    $cache = [];
+  ){
+    $this->controllerDiscovery = new ControllerDiscovery();
+    $this->cache = [];
+    $this->enableCache = false;
     if (file_exists(self::CACHE_ROUTE)) {
-      $cache = require self::CACHE_ROUTE;
+      $this->cache = require self::CACHE_ROUTE;
     }
+    $this->registerControllers($root);
+  }
 
-    foreach ($controllerFiles as $controllerFile) {
-      if ($controllerFile->isFile() && $controllerFile->getExtension() === "php") {
-        $namespace = null;
-        $class = null;
+  public function useRouteCache(): void
+  {
+    $this->enableCache = true;
+  }
 
-        if (filemtime(self::CACHE_ROUTE) >= filemtime($controllerFile->getPathname()) && !empty($cache)) {
+  private function registerControllers(string $root): void
+  {
+    foreach ($this->controllerDiscovery->scan($root) as $controller) {
+      $contextPath = "/";
+      $controllerAttribute = $controller->getAttributes(Controller::class);
+      if ($controllerAttribute) {
+        $contextPath = $controllerAttribute[0]->newInstance()->contextPath;
+      }
+      $contextPath = $this->normalizeURI($contextPath);
+
+      foreach ($controller->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+        $route = $method->getAttributes(Path::class);
+        if (empty($route)) {
           continue;
         }
 
-        $fileStream = @fopen($controllerFile->getPathname(), 'r');
-        if (!$fileStream) {
-          continue;
-        }
-        while (($line = fgets($fileStream)) !== false) {
-          if (preg_match('/^namespace\s+(.+?);/m', $line, $m)) {
-            $namespace = trim($m[1]);
-          }
+        $routeInfo = $route[0]->newInstance();
+        $uri = $this->normalizeURI($routeInfo->uri);
+        $path = "$contextPath$uri";
 
-          if (preg_match('/^class\s+([A-Za-z0-9_]+)/', $line, $m)) {
-            $class = trim($m[1]);
-            break;
-          }
-        }
-        @fclose($fileStream);
+        $middlewares = $method->getAttributes(Middleware::class);
 
-        if (!$class) {
-          continue;
-        }
-        $classFullname = "$namespace\\$class";
-
-        $reflector = new ReflectionClass($classFullname);
-
-        $contextPath = "/";
-        $controllerAttribute = $reflector->getAttributes(Controller::class);
-        if ($controllerAttribute) {
-          $contextPath = $controllerAttribute[0]->newInstance()->contextPath;
-        }
-
-        if (!str_starts_with($contextPath, "/")) {
-          $contextPath = "/$contextPath";
-        }
-
-        if (str_ends_with($contextPath, "/")) {
-          $contextPath = substr($contextPath, 0, -1);
-        }
-
-        foreach ($reflector->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-          $route = $method->getAttributes(Path::class);
-          if (empty($route)) {
-            continue;
-          }
-
-          $routeInfo = $route[0]->newInstance();
-          $uri = $routeInfo->uri;
-          if (!str_starts_with($uri, "/")) {
-            $uri = "/$uri";
-          }
-
-          if (str_ends_with($uri, "/")) {
-            $uri = substr($uri, 0, -1);
-          }
-          $path = "$contextPath$uri";
-
-          $middlewares = $method->getAttributes(Middleware::class);
-
-          $cache[$path][$routeInfo->httpMethod->name] = [
-            "class" => $classFullname,
+        $this->cache[$path][$routeInfo->httpMethod->name] = [
+            "class" => $controller->name,
             "method" => $method->getName(),
-            "middlewares" => array_map(function ($middleware){
+            "middlewares" => array_map(function ($middleware) {
               /**@var Middleware $teste */
               $teste = $middleware->newInstance();
               return $teste->handler;
             }, $middlewares),
-            "args" =>  array_map(fn(ReflectionParameter $p) =>  [
-              "name" => $p->getName(),
-              "type" => $p->getType()->getName(),
-              "is_body" => !empty($p->getAttributes(Body::class)),
+            "args" => array_map(fn(ReflectionParameter $p) => [
+                "name" => $p->getName(),
+                "type" => $p->getType()->getName(),
+                "is_body" => !empty($p->getAttributes(Body::class)),
             ], $method->getParameters())
-          ];
-        }
+        ];
       }
     }
 
-    if (!is_dir(self::CACHE_DIR)) {
-      mkdir(self::CACHE_DIR);
-    }
+    if ($this->enableCache) {
+      if (!is_dir(self::CACHE_DIR)) {
+        mkdir(self::CACHE_DIR);
+      }
 
-    $content = "<?php\n\nreturn " . var_export($cache ?? [], true) . ";";
-    file_put_contents(self::CACHE_ROUTE, $content);
+      $content = "<?php\n\nreturn " . var_export($cache ?? [], true) . ";";
+      file_put_contents(self::CACHE_ROUTE, $content);
+    }
   }
 
   public function dispatch(): void
@@ -151,51 +117,11 @@ final class Kernel
       $uri = substr($uri, 0, -1);
     }
     $httpMethod = $_SERVER["REQUEST_METHOD"];
-    $uris = require self::CACHE_ROUTE;
-
-    $route = $uris[$uri] ?? null;
+    $uris = $this->cache;
 
     $pathVariables = [];
-    if (!$route) {
-      foreach ($uris as $path => $methods) {
-        $path = str_replace("/", "\/", $path);
-        preg_match_all("/{([^\/]+)}/", $path, $variables);
-        $path = preg_replace("/{[^\/]+}/", "([^\/]+)", $path);
-        $pattern = "/^$path$/";
-        if (preg_match_all($pattern, $uri, $values)) {
-          $route = $methods;
-          foreach ($variables[1] as $key => $variable) {
-            $pathVariables[$variable] = $values[$key + 1][0];
-          }
-          break;
-        }
-      }
-    }
-
-    header("Content-Type: application/json");
-    if (!$route) {
-      http_response_code(404);
-      echo json_encode([
-        "error" => "path not found",
-        "status" => "not_found",
-        "timestamp" => date(DATE_ATOM)
-      ]);
-      exit(0);
-    }
-
-    $method = $route[$httpMethod] ?? null;
-
-    if (!$method) {
-      http_response_code(405);
-      echo json_encode([
-        "error" => "'$httpMethod' method not allowed to path '$uri'",
-        "status" => "method_not_allowed",
-        "timestamp" => date(DATE_ATOM)
-      ]);
-      exit(0);
-    }
-    ini_set("display_errors", 0);
-    ini_set('log_errors', '1');
+    $routerManager = new RouterManager($uris);
+    $method = $routerManager->resolve($uri, $httpMethod, $pathVariables);
     try {
       $instance = new $method["class"]();
       $firstMiddleware = $method["middlewares"][0];
@@ -205,7 +131,6 @@ final class Kernel
       foreach ($args as $arg) {
         [
           "name" => $name,
-          "type" => $type,
           "is_body" => $isBody
         ] = $arg;
 
@@ -231,7 +156,6 @@ final class Kernel
         if (!is_null($value)) {
           $argsValue[$name] = $value;
         }
-        
       }
 
       $middleware = null;
